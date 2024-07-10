cd95dd380c6d6e32952a71f73d4af4888d657ccc
Dallengaller <dallengaller@gmail.com>
2024-07-11 01:00:52 +0200

    configuracion servidor

--- index.js
+++ index.js
@@ -12,6 +12,18 @@
 
 // CONFIGURACION RENDER
 // En caso de no indicar el PORT, se inicia con el servidor por defecto.
+// import express from 'express';
+
+// const app = express();
+
+// app.get("/home", (req, res) => {
+//   res.send("Hello World Express Js");
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`¡Servidor encendido en puerto ${port}!`));
+
+
 import express from 'express';
 
 const app = express();
@@ -21,4 +33,7 @@ app.get("/home", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`¡Servidor encendido en puerto ${port}!`));
+app.listen(port, () => {
+  console.log(`¡Servidor encendido en puerto ${port}!`);
+  console.log('Servidor iniciado correctamente');
+});
